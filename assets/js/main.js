412ab0935ab227b364a4f9284e3a56ad43677fe9
Md Mamun Mia <mamunahammedco59@gmail.com>
2024-03-08 21:52:29 +0600

    details modal accordion added

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,6 +105,48 @@
         })
 
 
+        //------------ Related Product slider -------------
+
+        $('.re__product__main__slider').owlCarousel({
+            loop: false,
+            margin: 20,
+            nav: true,
+            navText: ['<i class="far fa-chevron-left"></i>', '<i class="far fa-chevron-right"></i>'],
+            dot: false,
+            items: 7,
+            slideToScroll: 1,
+            smartSpeed: 700,
+            responsive: {
+                320: {
+                    items: 2,
+                    margin: 10,
+                    nav: false,
+                },
+                450: {
+                    items: 3,
+                    margin: 10,
+                    nav: false,
+                },
+                575: {
+                    items: 3,
+                    nav: false,
+                },
+                768: {
+                    items: 4,
+                },
+                992: {
+                    items: 5,
+                },
+                1100: {
+                    items: 5,
+                },
+                1200: {
+                    items: 6,
+                },
+            }
+        })
+
+
         //------------ Category slider -------------
 
         $('.cat__main__slider').owlCarousel({
